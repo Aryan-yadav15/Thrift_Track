d0de99bb5427ab5a769305deece1493d53441109
Aryan Yadav <a2003yadav@gmail.com>
2024-01-07 14:18:54 +0530

    build chages

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,55 +1,42 @@
-import Link from 'next/link'
-import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const navIcons = [
-    { src: '/assets/icons/black-heart.svg' , alt: 'heart'},
-    { src: '/assets/icons/user.svg' , alt: 'user'},
-
+  { src: '/assets/icons/search.svg', alt: 'search' },
+  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+  { src: '/assets/icons/user.svg', alt: 'user' },
 ]
 
 const Navbar = () => {
-    const handleClick = () => {
-        // Perform the action when the logo is clicked
-        alert('Logo clicked!'); // You can replace this with your desired action
-      };
   return (
-    <header className='w-full border-y border-y-[#E4E4E4]'>
-        <nav className='nav'>
-            <Link href='/' className='flex items-center gap-1'>   
-                <Image 
-                    src='/assets/icons/logo.svg'
-                    width={27}
-                    height={27}
-                    alt='logo'
-                />
-                <p className='nav-logo'>
-                    Thrift<span className='text-primary'>Track</span>
-                </p>
-            </Link>
-            <div className='flex item-center gap-10'>
-            
-            <Link href='/' className='flex items-center gap-1'>   
-                <Image 
-                    src='/assets/icons/search.svg'
-                    width={28}
-                    height={28}
-                    alt='search_icon_nav'
-                    className='object-contain'
-                />
-            </Link>
-                {navIcons.map((icon)=>(
-                    <Image
-                        key = {icon.alt}
-                        src={icon.src}
-                        alt={icon.alt}
-                        width={28}
-                        height={28}
-                        className='object-contain'
-                    />
-                ))}
-            </div>
-        </nav>
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image 
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+
+          <p className="nav-logo">
+            Price<span className='text-primary'>Wise</span>
+          </p>
+        </Link>
+
+        <div className="flex items-center gap-5">
+          {navIcons.map((icon) => (
+            <Image 
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+              className="object-contain"
+            />
+          ))}
+        </div>
+      </nav>
     </header>
   )
 }
