1d8cc41750a8673e24bd76edf5d656c4e847b854
Aryan Yadav <a2003yadav@gmail.com>
2024-01-07 14:25:12 +0530

    // styles.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter,Space_Grotesk } from 'next/font/google'
-import './styles.css'
+import './/styles.css'
 import Navbar from '@/Components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
